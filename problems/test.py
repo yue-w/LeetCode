fc81e2b2fc4e1e3e87ab9dcd3874807276884851
Yue Wang <wyue@pm.me>
2022-07-26 20:29:30 -0400

    Daily update

--- problems/test.py
+++ problems/test.py
@@ -1,17 +1,13 @@
 
 #%%
-def bit_count_one(n):
-    print(f'binary: {bin(n)}')
-    count = 0
-    while n:
-        count += 1
-        n &= n - 1
-    return count
+def test():
+    def fun():
+        a.append(1)
+    
+    a = []
+    fun()
+    print(a)
 
-bit_count_one(1234)
-bit_count_one(0)
-bit_count_one(4321)
-bit_count_one(1)
-bit_count_one(3654)
+test()
 
-
+# %%
